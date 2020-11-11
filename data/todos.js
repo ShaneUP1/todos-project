19b33693ca9797b1be11ebaeebe5bf10fcdf36e3
Shane Upchurch <shaneupchurch@gmail.com>
2020-11-10 18:31:21 -0600

    tables and data updated and get working

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    completed: false,
+    todo: 'wash the dishes',
+    user_id: 1
+  },
+  {
+    completed: false,
+    todo: 'vacuum the house',
+    user_id: 1
+  },
+  {
+    completed: false,
+    todo: 'clean the bathroom',
+    user_id: 1
+  }
+];
